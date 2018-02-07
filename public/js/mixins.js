bd44194fd8f1ab372110aef560714eced8b2d513
aricallen <aric.allen2@gmail.com>
2018-02-06 20:53:06 -0800

    hideable mixin helper

---
+++ public/js/mixins.js
@@ -0,0 +1,21 @@
+(() => {
+  const hideable = ({ elem, isHiddenClass = 'is-hidden' }) => {
+    const mixin = {
+      show() {
+        elem.classList.remove(isHiddenClass);
+        return this;
+      },
+
+      hide() {
+        elem.classList.add(isHiddenClass);
+        return this;
+      },
+    };
+
+    return mixin;
+  };
+
+  const Mixins = { hideable };
+
+  window.Mixins = Mixins;
+})();
